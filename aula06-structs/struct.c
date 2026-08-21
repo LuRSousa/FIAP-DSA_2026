54654838d02877d4a25dc9a0946f6d74ea0d5b33
LuRSousa <lucasrs.tech@gmail.com>
2026-08-21 09:45:18 -0300

    adicionando structs com dependencia

--- aula06-structs/struct.c
+++ aula06-structs/struct.c
@@ -4,6 +4,14 @@
 // typedef (unsined int) (uint) -> renomeia o tipo de variavel
 typedef unsigned int uint;
 
+typedef struct
+{
+    char rua[50];
+    uint numero;
+    char bairro[50];
+    char cidade[50];
+} Endereco;
+
 // Definição da estrutura
 typedef struct
 {
@@ -15,20 +23,13 @@ typedef struct
     // email, data de nascimento, turma...
 } Aluno;
 
-typedef struct{
-    char rua[50];
-    uint numero;
-    char bairro[50];
-    char cidade[50];
-}Endereco;
-
 int main()
 {
     // Declaração  de uma variável do tipo struct
     Aluno aluno[3];
 
     aluno[0].rm = 123456;
-    strcpy(aluno[0].nome, "Claudio Abreu");
+    strcpy(aluno[0].nome, "Claudio Abreu Aura da Silva");
 
     // aluno[0].nota = {8.7, 9.1, 3.5};
     aluno[0].nota[0] = 8.7;
@@ -45,9 +46,15 @@ int main()
 
     float media = soma_notas / qnt_notas;
 
+    strcpy(aluno[0].endereco.rua, "Av. Paulista");
+    aluno[0].endereco.numero = 67;
+    strcpy(aluno[0].endereco.bairro, "Bela Vista");
+    strcpy(aluno[0].endereco.cidade, "São Paulo");
+
     printf("RM: %i\n", aluno[0].rm);
     printf("Nome: %s\n", aluno[0].nome);
     printf("Media: %.1f\n", media);
+    printf("Endereço: %s %i, %s - %s", aluno[0].endereco.rua, aluno[0].endereco.numero, aluno[0].endereco.bairro, aluno[0].endereco.cidade);
 
     return 0;
 }
