3819ae83e067f80db3cec5d1500530439d249d3b
LuRSousa <lucasrs.tech@gmail.com>
2026-09-18 09:51:44 -0300

    adicionando print para visualizacao da recursividade

--- aula08-recursividade/exercicio4.c
+++ aula08-recursividade/exercicio4.c
@@ -9,15 +9,17 @@ int maior(int v[], int n)
 
     if (v[n - 1] > anterior)
     {
+        printf("anterior --> %d | v[n-1] --> %d\n", anterior, v[n - 1]);
         return v[n - 1];
     }
 
+    printf("anterior --> %d | v[n-1] --> %d\n", anterior, v[n - 1]);
     return anterior;
 }
 
 int main()
 {
-    int v[] = {10, 20, 70, 30, 40, 50};
+    int v[] = {10, 70, 20, 30, 85, 40, 50};
     int n = sizeof(v) / sizeof(v[0]);
 
     printf("%d\n", maior(v, n));
